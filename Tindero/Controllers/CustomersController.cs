2adc95b776a869f981d9539fa7286e3c71efce86
Leonardo T. Uy Jr <leonardouyjr@gmail.com>
2020-10-28 22:22:19 +0800

    - Show Customers and its details when clicked

--- Tindero/Controllers/CustomersController.cs
+++ Tindero/Controllers/CustomersController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tindero.Models;
+using Tindero.ViewModel;
 
 namespace Tindero.Controllers
 {
     public class CustomersController : Controller
     {
         // GET: Customers
-        public ActionResult Index()
+
+        private IEnumerable<Customer> GetCustomers()
         {
-            return View();
+            var customers = new List<Customer>
+            {
+                new Customer {Id=1, FirstName = "Leonardo Jr.", LastName = "Uy", Birthdate = new DateTime(1978, 08, 24)},
+                new Customer {Id=2, FirstName = "Mary Rose", LastName = "Uy", Birthdate =  new DateTime(1977, 04, 12)},
+                new Customer {Id=3, FirstName = "Leonardo III", LastName = "Uy", Birthdate = new DateTime(2001, 10, 25)},
+                new Customer {Id=4, FirstName = "Keyanna Gael", LastName = "Uy", Birthdate = new DateTime(2003, 06, 13)}
+            };
+
+            return customers;
+        }
+        public ActionResult Show()
+        {
+            var customer = GetCustomers();            
+            return View(customer);
+        }
+
+        public ActionResult ShowDetails(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
         }
     }
 }
---
+++ Tindero/ViewModel/ShowCustomersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tindero.Models;
+
+namespace Tindero.ViewModel
+{
+    public class ShowCustomersViewModel
+    {
+        public List<Customer> Customers { get; set; }
+    }
+}
