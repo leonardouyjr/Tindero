93d2d6ac9da606072eb7e1f9e444645144764fe9
Leonardo T. Uy Jr <leonardouyjr@gmail.com>
2020-10-31 23:46:43 +0800

    Relate customer order details to a product

---
+++ Tindero/Migrations/202010311545388_AddProductToCustomerOrderDetails.cs
@@ -0,0 +1,22 @@
+﻿namespace Tindero.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProductToCustomerOrderDetails : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.CustomerOrderDetails", "ProductId", c => c.Int(nullable: false));
+            CreateIndex("dbo.CustomerOrderDetails", "ProductId");
+            AddForeignKey("dbo.CustomerOrderDetails", "ProductId", "dbo.Products", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CustomerOrderDetails", "ProductId", "dbo.Products");
+            DropIndex("dbo.CustomerOrderDetails", new[] { "ProductId" });
+            DropColumn("dbo.CustomerOrderDetails", "ProductId");
+        }
+    }
+}
--- Tindero/Models/CustomerOrderDetails.cs
+++ Tindero/Models/CustomerOrderDetails.cs
@@ -8,9 +8,13 @@ namespace Tindero.Models
     public class CustomerOrderDetails
     {
         public int Id { get; set; }
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+     
         public int CostPrice { get; set; }
         public int SalePrice { get; set; }
         public byte RegularDiscount { get; set; }
         public byte SeniorCitizenDiscount { get; set; }
+
     }
 }
