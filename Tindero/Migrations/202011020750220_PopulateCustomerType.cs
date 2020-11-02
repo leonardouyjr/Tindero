bf3741ecf7426b4a6218e49f6c28c38cd60985fe
Leonardo T. Uy Jr <leonardouyjr@gmail.com>
2020-11-02 16:16:55 +0800

    Seed CustomerTypes table

---
+++ Tindero/Migrations/202011020750220_PopulateCustomerType.cs
@@ -0,0 +1,24 @@
+﻿namespace Tindero.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCustomerType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("SET IDENTITY_INSERT CustomerTypes ON;");
+
+            Sql("INSERT INTO CustomerTypes(Id, Name) VALUES(1,'WalkIn')");
+            Sql("INSERT INTO CustomerTypes(Id, Name) VALUES(2,'Regular')");
+            Sql("INSERT INTO CustomerTypes(Id, Name) VALUES(3,'Silver')");
+            Sql("INSERT INTO CustomerTypes(Id, Name) VALUES(4,'Gold')");
+
+            Sql("SET IDENTITY_INSERT CustomerTypes OFF;");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
